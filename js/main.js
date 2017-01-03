39799bb359c4c3dcfd5c215b1d39ef84def34692
Leon <Strikerklm96@gmail.com>
2017-01-02 18:47:24 -0600

    Typescript Project Added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+
+function preload() {
+
+    //  You can fill the preloader with as many assets as your game requires
+
+    //  Here we are loading an image. The first parameter is the unique
+    //  string by which we'll identify the image later in our code.
+
+    //  The second parameter is the URL of the image (relative)
+    game.load.image('image1', 'assets/textures/image1.png');
+
+}
+
+function create() {
+
+    //  This creates a simple sprite that is using our loaded image and
+    //  displays it on-screen
+    game.add.sprite(0, 0, 'image1');
+
+}
