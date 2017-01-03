8cf017ad256c6ac60e9a367d1260456d6bc1069e
Leon <Strikerklm96@gmail.com>
2017-01-02 23:33:25 -0600

    Actually we have to have js for the server :-1:

---
+++ ts/main.js
@@ -0,0 +1,40 @@
+///<reference path="../lib/phaser.d.ts"/>
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    SimpleGame.prototype.preload = function () {
+        this.game.load.image('logo', 'assets/textures/image1.png');
+    };
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
+};
+var Greeter = (function () {
+    function Greeter(element) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+    Greeter.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
+    };
+    Greeter.prototype.stop = function () {
+        clearTimeout(this.timerToken);
+    };
+    return Greeter;
+}());
+//window.onload = () => {
+//    var el = document.getElementById('mainDiv');
+//    var greeter = new Greeter(el);
+//    greeter.start();
+//}; 
+//# sourceMappingURL=main.js.map
